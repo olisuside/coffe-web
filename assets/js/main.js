2ae8d6aff83bd1c20b324f5dbddb9bed595e96fa
Edy Susilo <masedysu69@gmail.com>
2022-06-29 00:58:48 +0700

    Upload files

---
+++ assets/js/main.js
@@ -0,0 +1,54 @@
+/*===== MENU SHOW Y HIDDEN =====*/
+const navMenu = document.getElementById("nav-menu"),
+  toggleMenu = document.getElementById("nav-toggle"),
+  closeMenu = document.getElementById("nav-close");
+
+// SHOW
+toggleMenu.addEventListener("click", () => {
+  navMenu.classList.toggle("show");
+});
+
+// HIDDEN
+closeMenu.addEventListener("click", () => {
+  navMenu.classList.remove("show");
+});
+
+/*===== MOUSEMOVE HOME IMG =====*/
+document.addEventListener("mousemove", move);
+function move(e) {
+  this.querySelectorAll(".move").forEach((layer) => {
+    const speed = layer.getAttribute("data-speed");
+
+    const x = (window.innerWidth - e.pageX * speed) / 120;
+    const y = (window.innerHeight - e.pageY * speed) / 120;
+
+    layer.style.transform = `translateX(${x}px) translateY(${y}px)`;
+  });
+}
+
+/*===== GSAP ANIMATION =====*/
+// NAV
+gsap.from(".nav__logo, .nav__toggle", { opacity: 0, duration: 1, delay: 2, y: 10 });
+gsap.from(".nav__item", { opacity: 0, duration: 1, delay: 2.1, y: 30, stagger: 0.2 });
+
+// HOME
+gsap.from(".home__title", { opacity: 0, duration: 1, delay: 1.6, y: 30 });
+gsap.from(".home__description", { opacity: 0, duration: 1, delay: 1.8, y: 30 });
+gsap.from(".home__button", { opacity: 0, duration: 1, delay: 2.1, y: 30 });
+gsap.from(".home__img", { opacity: 0, duration: 1, delay: 1.3, y: 30 });
+
+//About
+gsap.registerPlugin(ScrollTrigger);
+// For each images with class "animate-image" on page
+gsap.utils.toArray(".animate-image").forEach((el, index) => {
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: el,
+      start: "top top",
+      toggleActions: "play none none reverse",
+      markers: true,
+    },
+  });
+
+  tl.set(el, { transformOrigin: "center center" }).fromTo(el, { opacity: 0, scale: 0.8, y: "+=100" }, { opacity: 1, scale: 1, y: 0, duration: 1, immediateRender: false });
+});
